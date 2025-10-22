413b2c8c62a6afc7b771c06b78be74312151b376
SameerBhagtani01 <bhagtanisameer@gmail.com>
2025-10-22 21:44:58 +0530

    add logout to power menu, remove desktop entries, fix walker scroll

--- dotfiles/user_configs/.local/bin/power-menu.sh
+++ dotfiles/user_configs/.local/bin/power-menu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-items="\uf023  Lock\n\uf186  Suspend\n\uf2f1  Reboot\n\uf011  Shutdown"
-
+items="\uf023  Lock\n\uf08b  Logout\n\uf186  Suspend\n\uf2f1  Reboot\n\uf011  Shutdown"
 output=$(echo -e "$items" | walker --dmenu -H)
 
 case "$output" in
     *Lock)
         hyprlock
+        ;;
+    *Logout)
+        hyprctl dispatch exit 0
         ;;
     *Suspend)
         systemctl suspend
